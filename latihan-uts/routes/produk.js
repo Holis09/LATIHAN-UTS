d5443127eadf05f780e6d5d115c4af94d6f3128b
Holis09 <holisyogi399@gmail.com>
2024-10-25 18:52:12 +0700

    nurcholis

--- latihan-uts/routes/produk.js
+++ latihan-uts/routes/produk.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const produkController = require("/index");
+const produkController = require("/controllers.controllerProduk");
 router.get("/produk".produkController.index);
 module.exports =router;
